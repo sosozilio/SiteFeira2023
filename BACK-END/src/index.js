7139414e3cf7a740bbb5d991b6b680c2db848460
Macgithub13 <macgames444@gmail.com>
2023-09-05 10:37:59 -0300

    sim

---
+++ BACK-END/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import inscricaoController from './controller/inscricaoController.js';
+
+let server = express();
+server.use(cors())
+server.use(express.json())
+
+server.use(inscricaoController);
+server.use(login-admController);
+
+server.listen(process.env.PORT, () => console.log('API subiu'));
